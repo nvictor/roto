b8d97029dbbccad9a023669219dc0cde5cc47103
Victor Noagbodji <noagbodjivictor@gmail.com>
2026-08-31 21:31:27 -0400

    Update roto.lua

--- roto.lua
+++ roto.lua
@@ -33,17 +33,18 @@ cpal=1
 pal(pals[cpal])
 
 t=0
-w=240
-h=136
+w=240-1
+h=136-1
 
 function TIC()
  chgpal()
+
+ -- rotation
+ s=math.sin(t/45)
+ c=math.cos(t/45)
   
  for x=0,w do
   for y=0,h do
-   -- precalculate sin/cos
-   s=math.sin(t/45)
-   c=math.cos(t/45)
    -- pulsate around center
    x1=x-120+s*120
    y1=y-68+s*68
@@ -61,3 +62,33 @@ function TIC()
   pal(pals[cpal])
  end
 end
+
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
